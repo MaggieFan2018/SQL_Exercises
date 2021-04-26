4eb96cf64a8c5011d98ac379c05951aae382d13a
Mengqi Fan <fanm3@rpi.edu>
2021-04-26 17:38:14 -0400

    Create 1709. Biggest Window Between Visits.sql

---
+++ LeetCode/Window Function/1709. Biggest Window Between Visits.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT user_id, MAX(windows) AS biggest_window
+FROM (SELECT user_id, DATEDIFF(IFNULL(LEAD(visit_date, 1) OVER (PARTITION BY user_id ORDER BY visit_date),             '2021-1-1'), visit_date) AS windows
+     FROM UserVisits) t1
+GROUP BY 1
+ORDER BY 1;
