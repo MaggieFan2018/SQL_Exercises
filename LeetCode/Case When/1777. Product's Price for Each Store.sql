8ea5007ad53504f3a7c58320766f44e5d107135f
Mengqi Fan <fanm3@rpi.edu>
2021-07-02 12:54:16 -0400

    Create 1777. Product's Price for Each Store.sql

---
+++ LeetCode/Case When/1777. Product's Price for Each Store.sql
@@ -0,0 +1,7 @@
+
+#use case when clause
+SELECT product_id, SUM(CASE WHEN store = 'store1' THEN price END) AS store1, 
+                   SUM(CASE WHEN store = 'store2' THEN price END) AS store2,
+                   SUM(CASE WHEN store = 'store3' THEN price END) AS store3
+FROM Products
+GROUP BY 1;
